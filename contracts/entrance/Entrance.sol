07615964a4c4703cf3c801de68918900342dc960
Aler Denisov <aler.zampillo@gmail.com>
2018-04-10 14:31:28 +0300

    base entrance contract

---
+++ contracts/entrance/Entrance.sol
@@ -0,0 +1,55 @@
+pragma solidity 0.4.21;
+
+import "../router/Resolver.sol";
+
+contract Entrance { 
+  mapping (bytes32 => Resolver) public resolvers;
+
+  function register(string alias, address resolver) public {
+    bytes32 aliasHash = keccak256(alias);
+    require(resolvers[aliasHash] == address(0x0));
+    resolvers[aliasHash] = Resolver(resolver);
+  }
+
+  function getResolver(string alias) view public returns (address resolver) {
+    bytes32 aliasHash = keccak256(alias);
+    resolver = resolvers[aliasHash];
+  }
+
+  function () public {
+    bytes32 aliasHash;
+    uint result;
+    Resolver resolver;
+    address destination;
+    uint outsize;
+    bytes4 signature;
+
+    assembly {
+      aliasHash := calldataload(0)
+      signature := calldataload(32)
+    }
+
+    resolver = resolvers[aliasHash];    
+    (destination, outsize) = resolver.lookup(signature, msg.data);
+
+    assembly {
+      let size := sub(calldatasize, 32)
+      calldatacopy(mload(0x40), 32, size)
+      result := delegatecall(
+        sub(gas, 700), 
+        destination, 
+        mload(0x40),
+        size, 
+        mload(0x40), 
+        outsize)
+    }
+    // Throw if the call failed
+    if (result != 1) { 
+      revert();
+    }
+    // Pass on the return value
+    assembly {
+      return(mload(0x40), outsize)
+    }
+  }
+}
