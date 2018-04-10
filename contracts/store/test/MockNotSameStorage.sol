0009251570eddd850818f3324a132b312ac9d648
Aler Denisov <aler.zampillo@gmail.com>
2018-04-10 14:32:30 +0300

    rename and move storage mock contracts inside store folder

--- contracts/store/test/MockNotSameStorage.sol
+++ contracts/store/test/MockNotSameStorage.sol
@@ -1,6 +1,7 @@
-pragma solidity 0.4.19;
-import "../store/NotSameStorage.sol";
-import "../store/ExternalStorage.sol";
+pragma solidity ^0.4.18;
+
+import "../NotSameStorage.sol";
+import "../ExternalStorage.sol";
 
 
 contract MockNotSameStorage {
