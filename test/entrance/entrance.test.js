4e0ed044d75634eb386b102bbc3770c14b896135
Aler Denisov <aler.zampillo@gmail.com>
2018-04-13 13:09:37 +0300

    mock tests begin

--- test/entrance/entrance.test.js
+++ test/entrance/entrance.test.js
@@ -11,6 +11,16 @@ const expectThrow = require('../helpers/expectThrow')
 const Entrance = artifacts.require('./Entrance.sol')
 const Resolver = artifacts.require('./Resolver.sol')
 const TheAnswer = artifacts.require('TheAnswer.sol')
+const Multiplier = artifacts.require('Multiplier.sol')
+const Lost = artifacts.require('Lost.sol')
+const SimpleStore = artifacts.require('SimpleStore.sol')
+const ResolverAccessor = artifacts.require('ResolverAccessor.sol')
+const SenderChecker = artifacts.require('SenderChecker.sol')
+const One = artifacts.require('One.sol')
+const Two = artifacts.require('Two.sol')
+const List = artifacts.require('List.sol')
+const Thrower = artifacts.require('Thrower.sol')
+const PayableContract = artifacts.require('PayableContract.sol')
 
 const ethers = require('ethers')
 
@@ -96,5 +106,114 @@ contract('Entrance', () => {
 
       assert.equal(42, answer)
     })
+
+    it('should be able to pass along arguments', async () => {
+      const resolver = await Resolver.new(0)
+      const multiplier = await Multiplier.deployed()
+      await resolver.register('multiply(uint256,uint256)', multiplier.address, 32)
+      await entrance.register('multiplier', resolver.address)
+
+      const multiplierInterface = new ethers.Interface(Multiplier.abi)
+      const multiplierCall = multiplierInterface.functions.multiply(7, 3)
+      const callData = 'multiplier'.keccak256() + multiplierCall.data.substr(2)
+
+      const answer = multiplierCall.parse(await web3.eth.call({
+        to: entrance.address,
+        data: callData
+      }))
+
+      assert.equal(21, answer)
+    })
+
+    it('should be able to get multiple return values', async () => {
+      const resolver = await Resolver.new(0)
+      const lost = await Lost.deployed()
+      await resolver.register('getNumbers()', lost.address, 192)
+      await entrance.register('lost', resolver.address)
+
+      const lostInterface = new ethers.Interface(Lost.abi)
+      const lostCall = lostInterface.functions.getNumbers()
+      const callData = 'lost'.keccak256() + lostCall.data.substr(2)
+
+      const answer = lostCall.parse(await web3.eth.call({
+        to: entrance.address,
+        data: callData
+      }))
+
+      
+      assert.equal(answer[0].toNumber(), 4)
+      assert.equal(answer[1].toNumber(), 8)
+      assert.equal(answer[2].toNumber(), 15)
+      assert.equal(answer[3].toNumber(), 16)
+      assert.equal(answer[4].toNumber(), 23)
+      assert.equal(answer[5].toNumber(), 42)
+
+      // console.log(answer)
+      // console.log(lostCall.parse(answer))
+    })
+
+    it('should be able to register store contract', async () => {
+      const resolver = await Resolver.new(0)
+      const store = await SimpleStore.deployed()
+      await resolver.register('store(uint256)', store.address, 0)
+      await resolver.register('getStored()', store.address, 32)
+      await entrance.register('store', resolver.address)
+    })
+
+    it('should be able to store data', async () => {
+      const storeInterface = new ethers.Interface(SimpleStore.abi)
+      const storeTx = storeInterface.functions.store(42)
+      const storeTxData = 'store'.keccak256() + storeTx.data.substr(2)
+      await web3.eth.sendTransaction({
+        from: web3.eth.accounts[0],
+        to: entrance.address,
+        value: 0,
+        data: storeTxData,
+        gas: 300000
+      })
+    })
+
+    it('should be able to restore data', async () => {
+      const storeInterface = new ethers.Interface(SimpleStore.abi)
+      const restoreCall = storeInterface.functions.getStored()
+      const restoreCallData = 'store'.keccak256() + restoreCall.data.substr(2)
+      const answer = restoreCall.parse(await web3.eth.call({
+        to: entrance.address,
+        data: restoreCallData
+      }))
+
+      assert.equal(answer[0].toNumber(), 42)
+    })
   })
+
+  // describe('test', () => {
+  //   it('should return input string', async () => {
+  //     const aInstance = await A.new()
+
+  //     await aInstance.store('😀')
+  //     const aInterface = new ethers.Interface(A.abi)
+  //     const aCall = aInterface.functions.output()
+
+  //     const answer = await web3.eth.call({
+  //       to: aInstance.address,
+  //       data: aCall.data
+  //     })
+
+  //     console.log(answer, ethers.utils.toUtf8String(answer))
+  //     assert.equal('😀', ethers.utils.toUtf8String(answer))
+  //   })
+
+  //   it('should return from another contract', async () => {
+  //     const bInstance = await B.new()
+  //     await bInstance.store('Some string to store')
+  //     const bCall = new ethers.Interface(B.abi).functions.restore()
+
+  //     const answer = await web3.eth.call({
+  //       to: bInstance.address,
+  //       data: bCall.data
+  //     })
+
+  //     console.log(answer, ethers.utils.toUtf8String(answer))
+  //     assert.equal('Some string to store', ethers.utils.toUtf8String(answer))
+  //   })
 })
