326a964ab4b6d99b3c5dbc31c6f392d86c070058
Aler Denisov <aler.zampillo@gmail.com>
2018-04-17 21:42:33 +0300

    base musereum contract

---
+++ contracts/standard/Contract.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.19;
+import "./StandardLibrary.sol";
+import "./StringLibrary.sol";
+import "./AliasLibrary.sol";
+
+
+interface IEntrance {
+  function getOutsize(address _router, bytes4 _signature, bytes _data) view public returns (uint);
+}
+
+contract Contract {
+  using StandardLibrary for *;
+  using StringLibrary for *;
+  using AliasLibrary for *;
+  
+  address public resolver;
+  address public entrance;
+
+  function Contract(address _entrance) public {
+    require(_entrance != address(0x0));
+    require(_entrance.isContract());
+    entrance = _entrance;
+  }
+
+  modifier fromEntrance() {
+    require(msg.sender == entrance);
+    _;
+  }
+}
