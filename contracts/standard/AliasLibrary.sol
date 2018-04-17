4a1e4d80d4db3c70494d497fcaef4fddce4e2ea0
Aler Denisov <aler.zampillo@gmail.com>
2018-04-17 21:42:23 +0300

    aliases related lib

---
+++ contracts/standard/AliasLibrary.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.19;
+import "./StringLibrary.sol";
+
+
+library AliasLibrary {
+  using StringLibrary for *;
+
+  function isAliasSafe(string input) pure internal returns (bool) {
+    StringLibrary.slice memory slice = input.toSlice();
+
+    uint ptr = slice._ptr - 31;
+    uint end = ptr + slice._len;
+    while (ptr < end) {
+      uint8 b;
+      assembly { b := and(mload(ptr), 0xFF) }
+      if (b.charIsDot() || b.charIsNumber() || b.charIsLetter() || b.charIsLowLine() || b.charIsMinus()) {
+        ptr += 1;
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
